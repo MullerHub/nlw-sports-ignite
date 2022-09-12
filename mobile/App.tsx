92bf577daca6871558d5988bfe538d842fa75157
murilo muller <murilomuller@protonmail.com>
2022-09-11 22:26:31 -0300

    mobile config finished

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá mundo!</Text>
+
+      <Button title="Button 1" />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
