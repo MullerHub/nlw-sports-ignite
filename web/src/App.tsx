eb6360d9c2860ab2e41bfed885a99121e802d8c4
murilo muller <murilomuller@protonmail.com>
2022-09-13 19:10:06 -0300

    estilização semi pronta, falta detalhes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import './styles/main.css'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
 import logoImg from './assets/logo-nlw-esports.svg'
 
@@ -9,8 +10,86 @@ function App() {
         <img src={logoImg} alt="logo do site" />
 
         <h1 className="text-6xl text-white font-black mt-20">
-          Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+          Seu{' '}
+          <span className="text-transparent bg-nlw-gradient bg-clip-text">
+            duo
+          </span>{' '}
+          está aqui.
         </h1>
+
+        <div className="grid grid-cols-6 gap-6 mt-16">
+          <a
+            href=""
+            className="relative rounded-lg overflow-hidden bg-zinc-300"
+          >
+            <img src="/game1.png" />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className="relative">
+            <img src="/game2.png" />{' '}
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">Dota 2</strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className="relative">
+            <img src="/game3.png" />{' '}
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">CS:GO</strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className="relative">
+            <img src="/game5.png" />{' '}
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">
+                Apex legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className="relative">
+            <img src="/game6.png" />{' '}
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">Fortnite</strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className="relative">
+            <img src="/game7.png" />{' '}
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+              <strong className="font-bold text-white block">WoW</strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+        </div>
+
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-9">
+          <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+            <div>
+              <strong className="text-2xl text-white font-black block">
+                Não encontrou seu duo?
+              </strong>
+              <span className="text-zinc-400 block">Publique</span>
+            </div>
+
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
+              <MagnifyingGlassPlus size={24} />
+              Publicar anúncio
+            </button>
+          </div>
+        </div>
       </div>
     </>
   )
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,9 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png)",
+        galaxy: "url('/background-galaxy.png')",
         'nlw-gradient':
-          'linear-gradient(38deg, rgba(0,43,255,1) 0%, rgba(8,0,255,1) 7%, rgba(236,255,0,1) 39%, rgba(11,237,14,1) 100%)'
+          'linear-gradient(38deg, rgba(0,43,255,1) 0%, rgba(8,0,255,1) 7%, rgba(236,255,0,1) 39%, rgba(11,237,14,1) 100%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 69%)'
       }
     }
   },
