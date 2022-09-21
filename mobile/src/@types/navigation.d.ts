211a05a3b19303ccf1bfdb0f012d36b656c69b21
murilo muller <murilomuller@protonmail.com>
2022-09-21 19:25:45 -0300

    game style add

--- mobile/src/@types/navigation.d.ts
+++ mobile/src/@types/navigation.d.ts
@@ -1,12 +1,15 @@
+export interface GameParams {
+  id: string;
+  title: string;
+  bannerURL: string;
+}
+
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      game: {
-        id: string;
-        title: string;
-        bannerURL: string;
-      };
+      game: GameParams;
     }
   }
 }
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,13 +1,51 @@
-import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Background } from '../../components/Background'
-
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { styles } from './styles'
+import { GameParams } from '../../@types/navigation'
+import { TouchableOpacity, View, Image } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+import { THEME } from '../../theme'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
 
 export function Game() {
+  const navigation = useNavigation()
+  const route = useRoute()
+  const game = route.params as GameParams
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <>
       <Background>
-        <View style={styles.container}></View>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={handleGoBack}>
+              <Entypo
+                name="chevron-thin-left"
+                color={THEME.COLORS.CAPTION_300}
+                size={20}
+              />
+            </TouchableOpacity>
+
+            <Image source={logoImg} style={styles.logo} />
+            <View style={styles.right} />
+          </View>
+
+          <Image
+            source={{ uri: game.bannerURL }}
+            style={styles.cover}
+            resizeMode="cover"
+          />
+
+          <Heading
+            title={game.title}
+            subtitle="Conecte-se ce comece a jogar!"
+          />
+        </SafeAreaView>
       </Background>
       z
     </>
